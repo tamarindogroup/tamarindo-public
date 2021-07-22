3d4ccdd34008e367db2c0abbd29c7d5b4585f1cc
tamarindogroup <60480059+tamarindogroup@users.noreply.github.com>
2021-07-22 18:36:59 +0100

    Fixed input border

---
+++ js/ie11.js
@@ -0,0 +1,3 @@
+if (window.document.documentMode) {
+    alert ("It looks like you're using an unsupported browser. Some elements of this site may not work correctly. Please use a modern browser to continue using this site.")
+  }
