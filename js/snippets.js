a7f1981e5c7b8630ac5ab331d292e3235083e75e
tamarindogroup <60480059+tamarindogroup@users.noreply.github.com>
2021-11-02 11:04:28 +0000

    added flex gap support test

--- js/snippets.js
+++ js/snippets.js
@@ -29,3 +29,37 @@ for (var i = 0; i < linkedinShares.length; i++) {
   }
 
 })();
+
+
+
+/* check for flex gap support */
+
+// https://github.com/Modernizr/Modernizr/blob/master/feature-detects/css/flexgap.js
+
+function checkFlexGap() {
+	// create flex container with row-gap set
+	var flex = document.createElement("div");
+	flex.style.display = "flex";
+	flex.style.flexDirection = "column";
+	flex.style.rowGap = "1px";
+  
+	// create two, elements inside it
+	flex.appendChild(document.createElement("div"));
+	flex.appendChild(document.createElement("div"));
+  
+	// append to the DOM (needed to obtain scrollHeight)
+	document.body.appendChild(flex);
+	var isSupported = flex.scrollHeight === 1; // flex container should be 1px high from the row-gap
+	//flex.parentNode.removeChild(flex);
+  
+	return isSupported;
+  }
+  
+  console.log(checkFlexGap());
+  
+  if (checkFlexGap()) {
+	document.documentElement.classList.add("flexbox-gap");
+  } else {
+	document.documentElement.classList.add("no-flexbox-gap");
+  }
+  
